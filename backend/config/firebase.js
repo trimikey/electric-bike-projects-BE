539761a6002f9b536755d01eb4fb972785f3cc50
trimikey <leductri12321@gmail.com>
2025-10-01 18:07:59 +0700

    BE FE

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,9 +1,12 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+require("dotenv").config(); // Load biến môi trường từ .env
 
-
-admin.initializeApp({ 
-  credential: admin.credential.cert(serviceAccount),
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.GOOGLE_PROJECT_ID,
+    privateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+  }),
 });
 
 module.exports = admin;
