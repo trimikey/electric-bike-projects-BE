c17207597260efe32136a6d5a4ca014a38d7e368
trimikey <leductri12321@gmail.com>
2025-10-24 20:26:23 +0700

    ft: login gg

--- controllers/testDrive.controller.js
+++ controllers/testDrive.controller.js
@@ -3,16 +3,16 @@ const { TestDrive } = require("../models");
 
 
 exports.schedule = async (req, res) => {
-const { customer_id, dealer_id, vehicle_model_id, staff_id, schedule_at, notes } = req.body;
-const td = await TestDrive.create({
-id: uuidv46(),
-customer_id,
-dealer_id,
-vehicle_model_id,
-staff_id,
-schedule_at,
-status: "scheduled",
-notes,
-});
-res.status(201).json(td);
+    const { customer_id, dealer_id, vehicle_model_id, staff_id, schedule_at, notes } = req.body;
+    const td = await TestDrive.create({
+        id: uuidv46(),
+        customer_id,
+        dealer_id,
+        vehicle_model_id,
+        staff_id,
+        schedule_at,
+        status: "scheduled",
+        notes,
+    });
+    res.status(201).json(td);
 };
